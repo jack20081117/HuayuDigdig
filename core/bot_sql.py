8f2f9c1a8513eafbf8b1652d2c6776c78274d684
森岛船长 <1329913830@qq.com>
2024-05-18 20:11:03 +0800

    delete data

--- core/bot_sql.py
+++ core/bot_sql.py
@@ -3,37 +3,37 @@ import sqlite3
 createUser='create table users (' \
            'qq varchar(20),' \
            'schoolID varchar(10),' \
-           'money varchar(20),' \
+           'money int,' \
            'mineral varchar(1000),' \
-           'process_tech varchar(10),' \
-           'extract_tech varchar(10),' \
-           'digable varchar(10)' \
+           'process_tech double,' \
+           'extract_tech double,' \
+           'digable boolean' \
            ')'
 
 insertUser="insert into users " \
            "(qq,schoolID,money,mineral,process_tech,extract_tech,digable) " \
-           "values ('%s','%s','%s','%s','%s','%s','%s')"
+           "values ('%s','%s',%s,'%s',%f,%f,%s)"
 
 selectUserByID="select * from users where schoolID='%s'"
 selectUserByqq="select * from users where qq='%s'"
 
 updateMineByqq="update users set mineral='%s' where qq='%s'"
-updateDigableByqq="update users set digable='%s' where qq='%s'"
-updateDigable="update users set digable='%s'"
+updateDigableByqq="update users set digable=%s where qq='%s'"
+updateDigable="update users set digable=%s"
 
 createMine='create table mine (' \
-           'id varchar(10),' \
-           'time varchar(10)' \
+           'id int,' \
+           'time int' \
            ')'
 
 insertMine="insert into mine " \
            "(id,time) " \
-           "values ('%s','%s')"
+           "values (%d,%d)"
 
-selectTimeByID="select time from mine where id='%s'"
+selectTimeByID="select time from mine where id=%d"
 
-updateTimeByID="update mine set time='%s' where id='%s'"
-updateTime="update mine set time='%s'"
+updateTimeByID="update mine set time=%d where id=%d"
+updateTime="update mine set time=%d"
 
 def select(database,sql):
     with sqlite3.connect(database) as conn:
@@ -54,4 +54,5 @@ def execute(database,sql):
 if __name__ == '__main__':
     execute('data.db',createUser)
     execute('data.db',createMine)
-    execute('data.db',insertMine%('1','0'))
+    execute('data.db',insertMine%(1,0))
+    execute('data.db',insertMine%(2,0))
