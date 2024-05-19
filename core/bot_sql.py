89e6e67d946d56713d434bc0871c3c8bb00ef863
森岛船长 <1329913830@qq.com>
2024-05-19 09:12:41 +0800

    更改丰度错误

--- core/bot_sql.py
+++ core/bot_sql.py
@@ -2,65 +2,65 @@ import json
 import sqlite3
 import pymysql
 
-with open("./config.json", "r") as config:
-    config = json.load(config)
-env:str = config["env"]
-    
-if env == "prod":
-    with open("./mysql.json", "a") as config:
+with open("./config.json","r") as config:
+    config=json.load(config)
+env: str=config["env"]
+
+if env=="prod":
+    with open("./mysql.json","a") as config:
         pass
-    with open("./mysql.json", "r") as dbconfig:
-        dbconfig = json.load(dbconfig)
-    connection = pymysql.connect(host=dbconfig["host"], user=dbconfig["user"], password=dbconfig["password"], db=dbconfig["db"], charset="utf8")
-    mysqlcursor = connection.cursor()
-    
+    with open("./mysql.json","r") as dbconfig:
+        dbconfig=json.load(dbconfig)
+    connection=pymysql.connect(host=dbconfig["host"],user=dbconfig["user"],password=dbconfig["password"],
+                               db=dbconfig["db"],charset="utf8")
+    mysqlcursor=connection.cursor()
 
-createUserTable = 'create table users (' \
-           'qid varchar(10),' \
-           'schoolID varchar(5),' \
-           'money int,' \
-           'mineral varchar(1000),' \
-           'process_tech double,' \
-           'extract_tech double,' \
-           'digable boolean' \
-           ')'  # 建表users
-           
-createUserTableForMySQL = 'create table if not exists users (' \
-           'qid varchar(10),' \
-           'schoolID varchar(5),' \
-           'money int,' \
-           'mineral varchar(1000),' \
-           'process_tech double,' \
-           'extract_tech double,' \
-           'digable boolean' \
-           ') default charset utf8'  # 建表users
+createUserTable='create table users ('\
+                'qid varchar(10),'\
+                'schoolID varchar(5),'\
+                'money int,'\
+                'mineral varchar(1000),'\
+                'process_tech double,'\
+                'extract_tech double,'\
+                'digable boolean'\
+                ')'  # 建表users
 
-createUser = "insert into users " \
-           "(qid,schoolID,money,mineral,process_tech,extract_tech,digable) " \
-           "values ('%s','%s',%s,'%s',%f,%f,%s)" # 创建用户
-           # 拥有的矿石 加工科技 开采科技 是否能继续挖矿 (最后四个)
+createUserTableForMySQL='create table if not exists users ('\
+                        'qid varchar(10),'\
+                        'schoolID varchar(5),'\
+                        'money int,'\
+                        'mineral varchar(1000),'\
+                        'process_tech double,'\
+                        'extract_tech double,'\
+                        'digable boolean'\
+                        ') default charset utf8'  # 建表users
 
-selectUserBySchoolID="select * from users where schoolID='%s'" # 获取用户信息
-selectUserByQQ="select * from users where qid='%s'" # 获取用户信息
-selectUserByUserID = "select * from users where userid=%d"
+createUser="insert into users "\
+           "(qid,schoolID,money,mineral,process_tech,extract_tech,digable) "\
+           "values ('%s','%s',%s,'%s',%f,%f,%s)"  # 创建用户
+# 拥有的矿石 加工科技 开采科技 是否能继续挖矿 (最后四个)
+
+selectUserBySchoolID="select * from users where schoolID='%s'"  # 获取用户信息
+selectUserByQQ="select * from users where qid='%s'"  # 获取用户信息
+selectUserByUserID="select * from users where userid=%d"
 
 updateMoneyByQQ="update users set money=%d where qid='%s'"
-updateMineByQQ ="update users set mineral='%s' where qid='%s'"
-updateDigableByQQ ="update users set digable=%s where qid='%s'"
+updateMineByQQ="update users set mineral='%s' where qid='%s'"
+updateDigableByQQ="update users set digable=%s where qid='%s'"
 updateDigableAll="update users set digable=%s"
 
-createMineTable='create table mine (' \
-           'mineid int primary key,' \
-           'abundance float' \
-           ')'
+createMineTable='create table mine ('\
+                'mineid int primary key,'\
+                'abundance float'\
+                ')'
 
-createMineTableForMySQL='create table if not exists mine (' \
-           'mineid int primary key,' \
-           'abundance float' \
-           ')'
+createMineTableForMySQL='create table if not exists mine ('\
+                        'mineid int primary key,'\
+                        'abundance float'\
+                        ')'
 
-insertMine="insert into mine " \
-           "(mineID,abundance) " \
+insertMine="insert into mine "\
+           "(mineID,abundance) "\
            "values (%s,%s)"
 
 selectAbundanceByID="select abundance from mine where mineID=%d"
@@ -77,7 +77,7 @@ def select(database,sql):
         cursor.close()
     return res
 
-def execute(sql, mysql=False, *args):
+def execute(sql,mysql=False,*args):
     if not mysql:
         with sqlite3.connect("data.db") as conn:
             cursor=conn.cursor()
@@ -85,28 +85,25 @@ def execute(sql, mysql=False, *args):
             conn.commit()
             cursor.close()
     else:
-        mysqlcursor.execute(sql, args)
+        mysqlcursor.execute(sql,args)
         connection.commit()
-    
-    
 
-if __name__ == '__main__':
-    if env == "dev":
-        execute(createUserTable, False)
-        execute(createMineTable, False)
-        execute(insertMine, False, (1,0))
-        execute(insertMine, False, (2,0))
-        execute(insertMine, False, (3,0))
-        execute(insertMine, False, (4,0))
-        execute(insertMine, False, (5,0))
-        execute(insertMine, False, (6,0))
-    
-    elif env == "prod":
-        execute(createUserTableForMySQL, True)
-        execute(createMineTableForMySQL, True)
-        execute(insertMine, True, (1,0))
-        execute(insertMine, True, (2,0))
-        execute(insertMine, True, (3,0))
-        execute(insertMine, True, (4,0))
-        execute(insertMine, True, (5,0))
-        execute(insertMine, True, (6,0))
+if __name__=='__main__':
+    if env=="dev":
+        execute(createUserTable,False)
+        execute(createMineTable,False)
+        execute(insertMine,False,(1,0))
+        execute(insertMine,False,(2,0))
+        execute(insertMine,False,(3,0))
+        execute(insertMine,False,(4,0))
+        execute(insertMine,False,(5,0))
+        execute(insertMine,False,(6,0))
+    elif env=="prod":
+        execute(createUserTableForMySQL,True)
+        execute(createMineTableForMySQL,True)
+        execute(insertMine,True,(1,0))
+        execute(insertMine,True,(2,0))
+        execute(insertMine,True,(3,0))
+        execute(insertMine,True,(4,0))
+        execute(insertMine,True,(5,0))
+        execute(insertMine,True,(6,0))
